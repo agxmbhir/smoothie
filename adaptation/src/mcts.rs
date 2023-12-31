a3c9d05439ece3d7ec13fa97c31e2bf95de727c1
agxmbhir <agambhir5656@gmail.com>
2024-01-01 02:01:05 +0530

    Updated search tree

--- adaptation/src/mcts.rs
+++ adaptation/src/mcts.rs
@@ -4,53 +4,64 @@ pub struct mcts<T: Node> {
     root: T,
 }
 
+fn get_upper_confidence_bound(node: AdaptationNode, parent_visits: u32) -> f32 {
+    let c = 1.0;
+    let exploitation = node.get_state().id as f32;
+    let exploration = c * ((parent_visits.ilog10() / node.get_times_visited()) as f32).sqrt();
+    exploitation + exploration
+}
+
 impl<T: Node> mcts<T> {
     pub fn new() -> Self {
         mcts { root: T::new() }
     }
 
-    pub fn select(&self, node: T) -> T {
-        // Find an unexplored descendant of the given node.
-        if node.is_terminal() {
-            return node;
-        }
-        let current_node = node;
-        // Add a way to store the path
-
-        while !current_node.is_terminal() {
-            let children = current_node.get_children();
-            let mut unexplored_children = Vec::new();
-            for child in children {
-                if !child.is_explored() {
-                    unexplored_children.push(child);
+    fn select_child(&mut self, node: T) -> Self {
+        // If node is not fully expanded or terminal, return it.
+        // If the node is fully Expanded, return the child with the highest UCB and descend into it.
+        if self.is_terminal() {
+            return self.clone();
+        } else if !self.is_fully_expanded() {
+            return self.expand(); // Expand the node and return the most promising child.
+        } else {
+            let mut max_value = f32::NEG_INFINITY;
+            let mut max = node.get_children()[0].clone();
+            for child in node.get_children() {
+                let bound = get_upper_confidence_bound(*child.clone(), node.get_times_visited());
+                if bound > max_value {
+                    max_value = bound;
+                    max = child;
                 }
             }
-            if !unexplored_children.is_empty() {
-                // // if all the children have been explored
-                // // max value is negative infinity
-                // let mut max_value = f32::NEG_INFINITY;
-                // let mut max = unexplored_children[0];
-
-                // for child in unexplored_children {
-                //     let bound = get_upper_confidence_bound(child, current_node.get_times_visited());
-                //     if bound > max_value {
-                //         max_value = bound;
-                //         max = child;
-                //     }
-                // }
-                current_node = current_node.select_child();
-            } else {
-                // if there are unexplored children
-                let mut rng = rand::thread_rng();
-                let random_child = rng.gen_range(0, unexplored_children.len());
-                current_node = unexplored_children[random_child];
-            }
+            return max.select_child();
         }
-
-        get_upper_confidence_bound(current_node, current_node.get_times_visited());
     }
-    pub fn expand(&self, node: T) -> T {}
-    pub fn simulate(&self, node: T) -> T {}
+
+    fn expand(&mut self, node: T) -> Self {
+        let actions = self.get_actions();
+        let mut rng = rand::thread_rng();
+        let random_action = rng.gen_range(0..actions.len());
+
+        let new_state = State {
+            id: actions[random_action].id,
+            action: Box::new(actions[random_action].clone()),
+            reward: 0,
+        };
+        let child = AdaptationNode::new(new_state);
+        node.add_child(child);
+        child
+    }
+
+    pub fn simulate(&self, node: T) -> T {
+        let node = node.clone();
+        while True {
+            if node.is_terminal() {
+                return node;
+            }
+            let rand = rand::thread_rng().gen_range(0..node.get_actions().len());
+            node = node.get_actions()[rand].clone();
+        }
+    }
     pub fn backpropagate(&self, node: T) -> T {}
     pub fn run(&self, node: T) -> T {}
 }
--- adaptation/src/node.rs
+++ adaptation/src/node.rs
@@ -3,26 +3,34 @@
 use rand::Rng;
 
 // Implements the copy trait
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 pub struct State {
-    id: u32,     // id of the state
-    action: u32, // action taken to get to this state
-    reward: u32, // reward for getting to this state
+    id: u32,             // id of the state
+    action: Box<Action>, // action taken to get to this state
+    reward: u32,         // reward for getting to this state
 }
 
-pub trait Node {
-    fn new(&self, state: State) -> Self;
-    fn select_child(&self) -> Self; // Implemented as a trait so that it can be used for different problems.
+#[derive(Clone)]
+pub struct Action {
+    id: u32,
+    state: Box<State>,
+    probability: f32,
+}
 
-    // Sets the node as explored.
-    fn is_explored(&self) -> bool;
-    fn set_explored(&mut self);
+trait Node {
+    fn new(state: State) -> Self;
+
+    // Means all the children have been visited.
+    fn is_fully_expanded(&self) -> bool;
+    fn set_is_fully_expanded(&mut self);
 
     // Returns true if the node is a terminal node.
     fn is_terminal(&self) -> bool;
+    fn get_actions(&self) -> Vec<Action>; // Returns a vector of all the actions that can be taken from the node.
 
     // Returns a vector of all the children of the node.
     fn get_children(&self) -> Vec<Box<Self>>; // Uses Box to not have to worry about the size of the vector.
+    fn add_child(&mut self, child: AdaptationNode);
 
     // Managing the number of times a node has been visited.
     fn get_times_visited(&self) -> u32;
@@ -34,66 +42,33 @@ pub trait Node {
 }
 
 // Specific implementation of the node for UI adaptation problem.
+#[derive(Clone)]
 pub struct AdaptationNode {
     state: State,
     times_visited: u32,
+    children: Vec<Box<AdaptationNode>>,
     explored: bool,
 }
 
-fn get_upper_confidence_bound(node: AdaptationNode, parent_visits: u32) -> f32 {
-    let c = 1.0;
-    let exploitation = node.get_state().id as f32;
-    let exploration = c * ((parent_visits.ilog10() / node.get_times_visited()) as f32).sqrt();
-    exploitation + exploration
-}
-
 impl Node for AdaptationNode {
-    fn new(&self, state: State) -> Self {
+    fn new(state: State) -> Self {
         AdaptationNode {
             state,
             times_visited: 0,
+            children: Vec::new(),
             explored: false,
-        }
-    }
-    fn select_child(&self) -> Self {
-        // let c = 1.0;
-        // let exploitation = self.get_state().id as f32;
-        // let exploration = c * ((self.times_visited.ilog10() / node.get_times_visited()) as f32).sqrt();
-        // exploitation + exploration
-
-        let current_node = self;
-        let mut unexplored_children = Vec::new();
-        for child in self.get_children() {
-            if !child.is_explored() {
-                unexplored_children.push(child);
-            }
-        }
-
-        if unexplored_children.is_empty() {
-            let mut max_value = f32::NEG_INFINITY;
-            let mut max = unexplored_children[0];
-
-            for child in unexplored_children {
-                let bound = get_upper_confidence_bound(child, current_node.get_times_visited());
-                if bound > max_value {
-                    max_value = bound;
-                    max = child;
-                }
-            }
-            max
-        } else {
-            let mut rng = rand::thread_rng();
-            let random_child = rng.gen_range(0, unexplored_children.len());
-            unexplored_children[random_child]
         }
     }
 
     fn is_terminal(&self) -> bool {
-        self.get_children().is_empty()
+        self.get_actions().is_empty()
     }
 
     fn get_children(&self) -> Vec<Box<AdaptationNode>> {
         todo!()
+    }
+    fn add_child(&mut self, child: AdaptationNode) {
+        self.children.push(Box::new(child));
     }
 
     fn get_times_visited(&self) -> u32 {
@@ -101,22 +76,26 @@ impl Node for AdaptationNode {
     }
 
     fn get_state(&self) -> State {
-        self.state
+        self.state.clone()
     }
 
     fn set_state(&mut self, state: State) {
         self.state = state;
     }
 
-    fn is_explored(&self) -> bool {
-        self.explored
-    }
-
-    fn set_explored(&mut self) {
-        self.explored = true;
-    }
-
     fn increment_times_visited(&mut self) {
+        todo!()
+    }
+
+    fn is_fully_expanded(&self) -> bool {
+        todo!()
+    }
+
+    fn set_is_fully_expanded(&mut self) {
+        todo!()
+    }
+
+    fn get_actions(&self) -> Vec<Action> {
         todo!()
     }
 }
